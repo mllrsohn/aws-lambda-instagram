dab8bde760caa3f365761d69d3b4fc27e044cf40
Steffen Müller <steffen@mllrsohn.com>
2017-09-26 15:49:20 +0200

    added count and next_max_id

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ exports.handler = function(event, context, callback) {
     const params = {
         access_token: config.accessToken
     };
+
     if (context.min_id) {
         params.min_id = context.min_id;
     }
@@ -35,15 +36,21 @@ exports.handler = function(event, context, callback) {
     if (context.max_id) {
         params.max_id = context.max_id;
     }
+
+    if (context.count) {
+        context.count = context.count;
+    }
+
     request({
         host: 'api.instagram.com',
         path: `/v1/users/self/media/recent/?${stringify(params)}`
     })
         .then(data => {
             if (config.transform) {
-                data = config.transform(data);
+                data = config.transform(data.data);
             }
-            callback(null, data);
+            next_max_id = (data.pagination||{}).next_max_id || null;
+            callback(null, { data, next_max_id });
         })
         .catch(err => {
             callback('error getting data from api, check your lambda logs', []);
